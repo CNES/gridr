fix: fix window computation

doc/examples/example_grid_mask_chain_masked_tif.py
@@ -25,10 +25,10 @@ from scipy.interpolate import RegularGridInterpolator
 
 from gridr.core.grid.grid_commons import grid_full_resolution_shape
 from gridr.chain.grid_mask_chain import (
-        GridRIOMode,
         build_mask_chain,
         build_grid_mask_chain,
         )
+from gridr.io.common import GridRIOMode
 
 
 ALOGGER = logging.getLogger(__name__)

python/gridr/chain/grid_resampling_chain.py
@@ -324,7 +324,7 @@ def basic_grid_resampling_array(
         array_src_profile_2d = ArrayProfile(
             shape=(array_src_ds.height, array_src_ds.width),
             ndim=2,
-            dtype=np.dtype(array_src_ds.dtypes[array_src_bands[0]]))
+            dtype=np.dtype(array_src_ds.dtypes[array_src_bands[0]-1]))
         
         array_src_win_read, \
             array_src_win_marged,\
@@ -621,7 +621,7 @@ def basic_grid_resampling_chain(
     # The window contains the first and last index for each axis
     if window is None:
         logger.debug("No window given : the full grid is considered as window")
-        window = np.array(((0, full_shape_out[0]-1), (0, full_shape_out[1])))
+        window = np.array(((0, full_shape_out[0]-1), (0, full_shape_out[1]-1)))
     
     # If the window is given we have to check that it lies in the grid
     elif not window_check(full_profile_out, window):

tests/python/gridr/chain/test_grid_mask_chain.py
@@ -23,8 +23,8 @@ import rasterio
 from scipy.interpolate import RegularGridInterpolator
 
 from gridr.core.grid.grid_commons import grid_full_resolution_shape
+from gridr.io.common import GridRIOMode
 from gridr.chain.grid_mask_chain import (
-        GridRIOMode,
         build_mask_chain,
         build_grid_mask_chain,
         )